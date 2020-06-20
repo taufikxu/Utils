aef2ccf99f8c5dedd9a81ecc4515f8ffde8918bf
taufikxu <kunxu.thu@gmail.com>
2020-06-20 11:44:59 +0800

    update example, add metric_logger

--- example_test.py
+++ example_test.py
@@ -18,24 +18,29 @@ from ESM.sampling_mdsm import Annealed_Langevin_E, SS_denoise
 import Utils
 from Utils import config, flags
 
-matplotlib.use("Agg")
-FLAGS = flags.FLAGS
-config.load_config(FLAGS.config_file)
-if FLAGS.gpu.lower() not in ["-1", "none", Utils.config.notValid.lower()]:
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(FLAGS.gpu)
 
-torch.manual_seed(1234)
-torch.cuda.manual_seed(1235)
-np.random.seed(1236)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = True
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main():
+    matplotlib.use("Agg")
+    FLAGS = flags.FLAGS
+    config.load_config(FLAGS.config_file)
+    if FLAGS.gpu.lower() not in ["-1", "none", Utils.config.notValid.lower()]:
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(FLAGS.gpu)
 
-model = FLAGS.old_model
-dirname = os.path.dirname(model)
-basename = os.path.basename(model)
-config_path = os.path.join(dirname, "..", "source", "configs_dict.pkl")
-summary_path = os.path.join(dirname, "..", "summary")
-with open(config_path, "rb") as f:
-    new_dict = pickle.load(f)
-FLAGS.set_dict(new_dict)
+    torch.manual_seed(1234)
+    torch.cuda.manual_seed(1235)
+    np.random.seed(1236)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = FLAGS.old_model
+    dirname = os.path.dirname(model)
+    basename = os.path.basename(model)
+    config_path = os.path.join(dirname, "..", "source", "configs_dict.pkl")
+    summary_path = os.path.join(dirname, "..", "summary")
+    with open(config_path, "rb") as f:
+        new_dict = pickle.load(f)
+    FLAGS.set_dict(new_dict)
+
+if __name__ == "__main__":
+    main()
